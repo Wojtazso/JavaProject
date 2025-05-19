125687129ba5022509051a776d912b8247d7e47e
Wojtek <wojciechpizun1920@gmail.com>
2025-05-19 20:02:28 +0200

    sprawdzenie plikow

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
-    public static void main(String[] args) {};
+    public static void main(String[] args) {
+        System.out.println("Sprawdzenie działa plikow etcc");
+    };
     
 }
